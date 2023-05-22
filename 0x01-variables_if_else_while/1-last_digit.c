0666111bdb3f1b5678609c142fe3a7e8bd441ccc
Oghenetega Onokpasa <oghenetegaonokpasa1@gmail.com>
2023-05-22 14:20:43 -0700

    The last digit of n is greater than, equal to or less than the string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	i = n % 10;
 	if (i > 5)
-		printf("last digit of %d is %d and is greater than 5", n, i);
+		printf("Last digit of %d is %d and is greater than 5", n, i);
 	else if (i == 0)
 		printf("Last digit of %d is %d and is 0", n, i);
 	else
